49155b29150d244c4e6b37d337210ccd1eab04db
Iain Hamilton <iain@beakable.com>
2013-07-11 02:53:53 +0000

    Added basics dim/brighten for any RF  module containing 'lamp'

--- js/bpi/rf/serviceView.js
+++ js/bpi/rf/serviceView.js
@@ -13,6 +13,26 @@
     You should have received a copy of the GNU General Public License
     along with BeakPi.  If not, see <http://www.gnu.org/licenses/>. */
 
+/*
+RF Commands
+0x6 == All Lights Off
+0xE == Status = Off
+0x2 ==On
+0xA == Preset Dim
+0x1 == All Lights On
+0x9 == Hail Acknowledge
+0x5 == Bright
+0xD == Status = On
+0x7 == Extended Code
+0xF == Status Request
+0x3 == Off
+0xB == Preset Dim
+0x0 == All Units Off
+0x8 == Hail Request
+0x4 == Dim
+0xC == Extended Data (analog)
+*/
+
 define([
   "dojo/_base/declare",
   "dojo/_base/lang",
@@ -70,31 +90,65 @@ function(declare, lang, array, JSON, Deferred, domConst, _WidgetBase, _WidgetsIn
       var nodeArray = JSON.parse(nodes.Arguments.Argument[1]);
       var _self = this;
       array.forEach(nodeArray, lang.hitch(this, function(node){
-        if(node.name !=="") {
+        if (node.name !=="") {
           var btnPowerOn = new Button({
-            label: "Off",
+            label: "On",
             rfid: node.id,
             onClick: function(){
-              when(util.requestRF('<zbpPacket><Object>ZBP_Node</Object><methodName>Node_ClusterCommand</methodName><Arguments><Argument type="uchar" base="10">2</Argument><Argument type="ushort" base="10">'+this.rfid+'</Argument><Argument type="uchar" base="10">0</Argument><Argument type="ushort" base="10">0</Argument><Argument type="uchar" base="10">0</Argument><Argument type="QByteArray" base="10">[19,0]</Argument></Arguments>'+_self._session+'<id>'+this.rfid+'</id></zbpPacket>'), function(res) {
+              when(util.requestRF('<zbpPacket><Object>ZBP_Node</Object><methodName>Node_ClusterCommand</methodName><Arguments><Argument type="uchar" base="10">2</Argument><Argument type="ushort" base="10">'+this.rfid+'</Argument><Argument type="uchar" base="10">0</Argument><Argument type="ushort" base="10">0</Argument><Argument type="uchar" base="10">0</Argument><Argument type="QByteArray" base="10">[2]</Argument></Arguments>'+_self._session+'<id>'+this.rfid+'</id></zbpPacket>'), function(res) {
                 console.log("Response: " , res);
               });
             }
           });
+
           var btnPowerOff = new Button({
-            label: "On",
+            label: "Off",
             rfid: node.id,
             onClick: function(){
-              when(util.requestRF('<zbpPacket><Object>ZBP_Node</Object><methodName>Node_ClusterCommand</methodName><Arguments><Argument type="uchar" base="10">2</Argument><Argument type="ushort" base="10">'+this.rfid+'</Argument><Argument type="uchar" base="10">0</Argument><Argument type="ushort" base="10">0</Argument><Argument type="uchar" base="10">0</Argument><Argument type="QByteArray" base="10">[2,0]</Argument></Arguments>'+_self._session+'<id>'+this.rfid+'</id></zbpPacket>'), function(res) {
+              when(util.requestRF('<zbpPacket><Object>ZBP_Node</Object><methodName>Node_ClusterCommand</methodName><Arguments><Argument type="uchar" base="10">2</Argument><Argument type="ushort" base="10">'+this.rfid+'</Argument><Argument type="uchar" base="10">0</Argument><Argument type="ushort" base="10">0</Argument><Argument type="uchar" base="10">0</Argument><Argument type="QByteArray" base="10">[3]</Argument></Arguments>'+_self._session+'<id>'+this.rfid+'</id></zbpPacket>'), function(res) {
                 console.log("Response: " , _self._CURRENT);
                 _self._CURRENT++;
               });
             }
           });
 
-          domConst.place("<h3>" +  node.name + "</h3>", this._serviceView);
-          btnPowerOn.placeAt(this._serviceView);
-          domConst.place("<span> - </span>", this._serviceView);
-          btnPowerOff.placeAt(this._serviceView);
+          var btnPowerDim = new Button({
+            label: "Dim",
+            rfid: node.id,
+            onClick: function(){
+              when(util.requestRF('<zbpPacket><Object>ZBP_Node</Object><methodName>Node_ClusterCommand</methodName><Arguments><Argument type="uchar" base="10">2</Argument><Argument type="ushort" base="10">'+this.rfid+'</Argument><Argument type="uchar" base="10">0</Argument><Argument type="ushort" base="10">0</Argument><Argument type="uchar" base="10">0</Argument><Argument type="QByteArray" base="10">[4]</Argument></Arguments>'+_self._session+'<id>'+this.rfid+'</id></zbpPacket>'), function(res) {
+                console.log("Response: " , res);
+              });
+            }
+          });
+
+          var btnPowerBrighten = new Button({
+            label: "Brighten",
+            rfid: node.id,
+            onClick: function(){
+              when(util.requestRF('<zbpPacket><Object>ZBP_Node</Object><methodName>Node_ClusterCommand</methodName><Arguments><Argument type="uchar" base="10">2</Argument><Argument type="ushort" base="10">'+this.rfid+'</Argument><Argument type="uchar" base="10">0</Argument><Argument type="ushort" base="10">0</Argument><Argument type="uchar" base="10">0</Argument><Argument type="QByteArray" base="10">[5]</Argument></Arguments>'+_self._session+'<id>'+this.rfid+'</id></zbpPacket>'), function(res) {
+                console.log("Response: " , _self._CURRENT);
+                _self._CURRENT++;
+              });
+            }
+          });
+
+          if (node.name.toLowerCase().indexOf("lamp") === -1) {
+            domConst.place("<h3>" +  node.name + "</h3>", this._serviceView);
+            btnPowerOff.placeAt(this._serviceView);
+            domConst.place("<span> - </span>", this._serviceView);
+            btnPowerOn.placeAt(this._serviceView);
+          }
+          else {
+            domConst.place("<h3>" +  node.name + "</h3>", this._serviceView);
+            btnPowerOff.placeAt(this._serviceView);
+            domConst.place("<span> - </span>", this._serviceView);
+            btnPowerDim.placeAt(this._serviceView);
+            domConst.place("<span> - </span>", this._serviceView);
+            btnPowerBrighten .placeAt(this._serviceView);
+            domConst.place("<span> - </span>", this._serviceView);
+            btnPowerOn.placeAt(this._serviceView);
+          }
         }
       }));
     }
