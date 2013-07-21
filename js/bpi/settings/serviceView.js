cd03b45e0e67c87cc755b9b99ace4f3a37806518
Iain Hamilton <iain@beakable.com>
2013-07-21 22:19:01 +0000

    Updated to use improved command dfd handler

--- js/bpi/settings/serviceView.js
+++ js/bpi/settings/serviceView.js
@@ -38,26 +38,16 @@ function(declare, lang, array, Deferred, domConst, _WidgetBase, _WidgetsInTempla
 
     _intervalCurrentUsage: new timing.Timer(3000),
 
-    _filterText: function(result) {
-      if(result.text) {
-        return result.text;
-      }
-      else{
-        return result;
-      }
-    },
-
     _refreshCurrentUsage: function() {
       var _self = this;
       var dfd = new Deferred();
       when(util.getSystemUsage(), function(resData){
         when(util.command("/opt/vc/bin/vcgencmd measure_temp"), function(res){
           domConst.empty(_self._serviceViewCurrent);
-          systemData = _self._filterText(resData).split("|");
+          systemData = resData.split("|");
           domConst.place("<span>Current CPU Usage: " + systemData[0] + "%</span>", _self._serviceViewCurrent);
           domConst.place("<br /><span>Current Memory Usage: "  + systemData[2] + " / " + systemData[1] + " MB</span>", _self._serviceViewCurrent);
-          res = _self._filterText(res);
-          domConst.place("<br /><span>Current Pi Temperature: " + res.replace("temp=", "") + "</span>", _self._serviceViewCurrent);
+          domConst.place("<br /><span>Current Pi Temperature: " + res[0].replace("temp=", "") + "</span>", _self._serviceViewCurrent);
           dfd.resolve();
         });
       });
@@ -80,14 +70,11 @@ function(declare, lang, array, Deferred, domConst, _WidgetBase, _WidgetsInTempla
       when(this._refreshCurrentUsage(), function(){
         domConst.place("<br /><span>__________________________</span><br />", _self._serviceViewInfo);
         when(util.command("/opt/vc/bin/vcgencmd measure_volts"), function(res){
-          res = _self._filterText(res);
-          domConst.place("<br /><span>Volts: " + res.replace("volt=", "") + "</span>", _self._serviceViewInfo);
+          domConst.place("<br /><span>Volts: " + res[0].replace("volt=", "") + "</span>", _self._serviceViewInfo);
            when(util.command("/opt/vc/bin/vcgencmd get_mem arm"), function(res){
-            res = _self._filterText(res);
-            domConst.place("<br /><span>ARM Memory: " + res.replace("arm=", "") + "</span>", _self._serviceViewInfo);
+            domConst.place("<br /><span>ARM Memory: " + res[0].replace("arm=", "") + "</span>", _self._serviceViewInfo);
             when(util.command("/opt/vc/bin/vcgencmd get_mem gpu"), function(res){
-              res = _self._filterText(res);
-              domConst.place("<br /><span>GPU Memory: " + res.replace("gpu=", "") + "</span>", _self._serviceViewInfo);
+              domConst.place("<br /><span>GPU Memory: " + res[0].replace("gpu=", "") + "</span>", _self._serviceViewInfo);
               dfd.resolve();
             });
           });
